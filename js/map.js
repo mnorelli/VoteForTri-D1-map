f7539ec8bb95365ca561aad9fb1931637d25a341
Michael Norelli <mnorelli360@gmail.com>
2020-03-13 12:08:42 -0700

    adds geocoder with bbox and proximity parameters

--- js/map.js
+++ js/map.js
@@ -8,6 +8,23 @@ var map = new mapboxgl.Map({
   zoom: 12, // Starting zoom level
 });
 
-var marker = new mapboxgl.Marker() // initialize a new marker
-  .setLngLat(centerD1) // Marker [lng, lat] coordinates
-  .addTo(map); // Add the marker to the map
+// var marker = new mapboxgl.Marker() // initialize a new marker
+//   .setLngLat(centerD1) // Marker [lng, lat] coordinates
+//   .addTo(map); // Add the marker to the map
+
+var geocoder = new MapboxGeocoder({ // Initialize the geocoder
+  accessToken: mapboxgl.accessToken, // Set the access token
+  placeholder: 'Search in District 1',
+  mapboxgl: mapboxgl, // Set the mapbox-gl instance
+  marker: {color: 'orange'}, // https://docs.mapbox.com/mapbox-gl-js/example/point-from-geocoder-result/
+  bbox: [-122.30937,37.84214,-122.23715,37.89838],
+  proximity: { // centerD1
+    longitude: -122.246048,
+    latitude: 37.852483}
+});
+
+// Add the geocoder to the map
+map.addControl(geocoder,'top-left');
+
+// Add zoom and rotation controls to the map.
+map.addControl(new mapboxgl.NavigationControl());
