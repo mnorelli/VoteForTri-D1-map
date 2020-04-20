77c88064d9f9ff2cf8d08762febb0b34ba053154
Michael Norelli <mnorelli360@gmail.com>
2020-04-20 16:38:07 -0700

    removes pseudocode and comment blocks

--- js/map.js
+++ js/map.js
@@ -2,53 +2,24 @@ var centerD1 = [-122.246048,37.852483]
 
 mapboxgl.accessToken = TOKEN;
 var map = new mapboxgl.Map({
-  container: 'map', // Container ID
-  style: 'mapbox://styles/mapbox/streets-v11', // Map style to use
-  center: centerD1, // Starting position [lng, lat]
-  zoom: 12.6, // Starting zoom level
+  container: 'map', 
+  style: 'mapbox://styles/mapbox/streets-v11',
+  center: centerD1, 
+  zoom: 12.6, 
 });
 
-var geocoder = new MapboxGeocoder({ // Initialize the geocoder
-  accessToken: mapboxgl.accessToken, // Set the access token
+var geocoder = new MapboxGeocoder({ 
+  accessToken: mapboxgl.accessToken, 
   placeholder: 'Search in District 1',
-  mapboxgl: mapboxgl, // Set the mapbox-gl instance
-  marker: {color: 'rgba(76,0,53,1)'}, // https://docs.mapbox.com/mapbox-gl-js/example/point-from-geocoder-result/
+  mapboxgl: mapboxgl,
+  marker: {color: 'rgba(76,0,53,1)'}, 
   bbox: [-122.29513,37.81106,-122.19705,37.89389]
 });
 
-// Add the geocoder to the map
 map.addControl(geocoder,'top-left');
-
-// Add zoom and rotation controls to the map.
 map.addControl(new mapboxgl.NavigationControl());
 
-// Add a GeoJSON polygon
-// https://docs.mapbox.com/mapbox-gl-js/example/geojson-polygon/
-// https://docs.mapbox.com/mapbox-gl-js/example/timeline-animation/  adding GeoJSON from file
-
-
 map.on('load', function() {
-
-
-    //     var data = $.ajax({
-    //       url:"https://raw.githubusercontent.com/mnorelli/VoteForTri-D1-map/master/geodata/District1.geojson",
-    //       dataType: "json",
-    //       success: console.log("District data loaded\n" + data),
-    //       error: function (xhr) {
-    //         alert(xhr.statusText)
-    //       }
-    //     })
-
-    // $.when(data).done(function() {
-
-//console.log("before d3")
-
-  // d3.json(
-  //     'https://raw.githubusercontent.com/mnorelli/VoteForTri-D1-map/master/geodata/District1.geojson',
-  //     function(err, data) {
-  //         if (err) throw err;
-          console.log(data);
-          console.log("District data loaded");
 
           map.addSource('district1', {
               'type': 'geojson',
@@ -66,11 +37,6 @@ map.on('load', function() {
               }
           });
 
-
-  //     }
-  // );
-
-//console.log("after d3")
 
 });
 
