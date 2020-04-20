a1b07504ae85b01a434e5d170fb51cf5906b0e9f
Michael Norelli <mnorelli360@gmail.com>
2020-04-20 13:06:39 -0700

    corrects order of bbox coords

--- js/map.js
+++ js/map.js
@@ -13,7 +13,7 @@ var geocoder = new MapboxGeocoder({ // Initialize the geocoder
   placeholder: 'Search in District 1',
   mapboxgl: mapboxgl, // Set the mapbox-gl instance
   marker: {color: 'rgba(76,0,53,1)'}, // https://docs.mapbox.com/mapbox-gl-js/example/point-from-geocoder-result/
-  bbox: [-122.29513,37.89389,-122.19705,37.81106]
+  bbox: [-122.29513,37.81106,-122.19705,37.89389]
 });
 
 // Add the geocoder to the map
