2da779ebca698f37b67468ac4a0ed3f233acbd0e
Michael Norelli <mnorelli360@gmail.com>
2020-05-01 17:33:46 -0700

    adds geocoded addr as part of query to City data

--- js/map.js
+++ js/map.js
@@ -16,6 +16,22 @@ var geocoder = new MapboxGeocoder({
   bbox: [-122.29513,37.81106,-122.19705,37.89389]
 });
 
+// ArcGIS REST query parts
+var reqTarget = 'http://gisapps1.mapoakland.com/oakgis/rest/services/Prod/CouncilDistricts/MapServer/identify';
+var reqType = 'f=json';
+var reqTol = 'tolerance=1';
+var reqImgDisp = 'imageDisplay=1440,217,96';
+var reqGType = 'geometryType=esriGeometryPoint';
+var reqGeomRet = 'returnGeometry=false';
+var reqLyrs = 'layers=top:0';
+var reqURLpart1 = reqTarget + '?'
+             + reqGType + '&'
+             + reqLyrs + '&'
+             + reqTol + '&'
+             + reqImgDisp + '&'
+             + reqGeomRet + '&'
+             + reqType
+
 map.addControl(geocoder,'top-left');
 map.addControl(new mapboxgl.NavigationControl());
 
@@ -39,22 +55,45 @@ map.on('load', function() {
 
 // https://stackoverflow.com/questions/57677373/how-to-get-mapbox-geocoder-result-place-country-text-names-into-a-javascript-fun
   geocoder.on('result', function(e) {
-      console.log(e.result.address + ', ' + e.result.text);
-      console.log(e)
+      console.log(e.result.address + ' ' + e.result.text);
+      var reqLon = e.result.geometry.coordinates[0]
+      var reqLat = e.result.geometry.coordinates[1]
 
-      var request = new XMLHttpRequest()
+      // var reqSpRef = '102643';  // 2227  NAD83 CA State Plane Zone 3 feet
+      // var reqExt = 'mapExtent=6043165,2122759,6071995,2152385';
+      // var reqGeom = 'geometry=6051461,2130232'; 
+
+      var reqSpRef = 'sr=4326';  // WGS84
+      var reqExt = 'mapExtent=-122.294970,37.811059,-122.197046,37.893884';
+      var reqGeom = 'geometry=' + reqLon + ',' + reqLat; 
+
+      var reqURL = reqURLpart1 + '&'
+                   + reqGeom + '&'
+                   + reqSpRef + '&'
+                   + reqExt
+
+      var request = new XMLHttpRequest();
 
       // Open a new connection, using the GET request on the URL endpoint
-      request.open('GET', 'http://gisapps1.mapoakland.com/oakgis/rest/services/Prod/CouncilDistricts/MapServer/identify?f=json&tolerance=1&imageDisplay=1440,217,96&geometry={"x":6055964.00,"y":2131689.25}&geometryType=esriGeometryPoint&sr=102643&mapExtent=6035940.637268476,2142924.21076519,6079895.748164229,2149548.0017821193&layers=all:0', true)
+      request.open('GET', reqURL, true);
 
       request.onload = function() {
         // Begin accessing JSON data here
-        console.log(JSON.parse(this.response))
+        var oakgis_obj = JSON.parse(this.response);
+
+        console.log("Your address is in " + oakgis_obj.results[0].attributes.FULLNAME);
       }
 
       // Send request
-      request.send()
+      request.send();
   });
 
 });
+
+
+
+
+
+
+
 
